3e087db6f142e7973028da4c9465fe902dfd5461
Oleksandr Petrov <petr.ov@icloud.com>
2016-08-19 17:06:53 +0300

    Flask-Shit-o-lator v.4.1
    With simple jinja, static files and fixed code

--- shit-o-lator.py
+++ shit-o-lator.py
@@ -2,46 +2,46 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def shit_o_lator():
     return render_template('shit-o-lator.html')
 
-@app.route('/calculate', methods=['POST'])
+@app.route('/calculate', methods = ['POST'])
 def calculate():
     # Get the form data
-    dig1 = request.form['dig1']
-    dig2 = request.form['dig2']
-    oper = request.form['oper']
+    digit1 = request.form['digit1']
+    digit2 = request.form['digit2']
+    operation = request.form['operation']
 
-    # check whether digits
+    # Check whether digits & dividing on zero
     try:
-        float(dig1)
-        float(dig2)
-    except (ValueError):
-        return render_template('shit-o-lator.html', message = "Please, input the digits!")
-    # Check dividing on zero
-    try:
-        float(dig1) / float(dig2)
-    except (ZeroDivisionError):
-        return render_template('shit-o-lator.html', message = "You can't divide on zero!")
+        digit1 = float(digit1)
+        digit2 = float(digit2)
+        digit1 / digit2
+    except (ValueError, ZeroDivisionError) as exc:
+        if str(type(exc)) == "<type 'exceptions.ValueError'>":
+            return render_template('message.html', message = "Please, input the digits!")
+        else:
+            return render_template('message.html', message = "You can't divide on zero!")
 
     # Calculation
-    oper_kind = {'+': float(dig1) + float(dig2),
-                 '-': float(dig1) - float(dig2),
-                 '*': float(dig1) * float(dig2),
-                 '/': float(dig1) / float(dig2)}
-    for n in oper_kind:
-        if n == oper:
-            summ = oper_kind[n]
+    operation_kind = {'+': digit1 + digit2,
+                      '-': digit1 - digit2,
+                      '*': digit1 * digit2,
+                      '/': digit1 / digit2}
+    for n in operation_kind:
+        if n == operation:
+            summ = operation_kind[n]
 
+    # Check wether the result is integer
     if summ - int(summ) == 0:
         message = "The result is: {}".format(int(summ))
     else:
         message = "The result is: {0:.2f}".format(summ)
     # Output
-    return render_template('shit-o-lator.html', message = message)
+    return render_template('message.html', message = message)
 
 if __name__ == '__main__':
     app.run()
