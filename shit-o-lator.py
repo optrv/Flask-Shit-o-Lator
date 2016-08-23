6e457564a175bbd87fb2e71e7cdecee3a90f745c
Oleksandr Petrov <petr.ov@icloud.com>
2016-08-23 17:53:21 +0300

    Flask-Shit-o-lator v.4.2
    With custom exceptions, decimals and little bit more jinja)

---
+++ shit-o-lator.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+from decimal import Decimal, InvalidOperation
+
+app = Flask(__name__, static_url_path='/static')
+
+class EmptyFieldError(Exception):
+    """Raise when the input field is empty"""
+
+class DivZeroError(Exception):
+    """Raise when dividing on zero"""
+
+@app.route('/')
+def shit_o_lator():
+    return render_template('shit-o-lator.html')
+
+@app.route('/calculate', methods = ['POST'])
+def calculate():
+    """Get the form data"""
+    digit1 = request.form['digit1']
+    digit2 = request.form['digit2']
+    operation = request.form['operation']
+
+    try:
+        # Check whether the fields are not empty
+        if digit1 == "" or digit2 == "":
+            raise EmptyFieldError("EmptyFieldError")
+
+        # Check dividing on zero
+        if operation == '/' and digit2 == '0':
+            raise DivZeroError("DivZeroError")
+
+        # Check whether digits
+        digit1 = Decimal(digit1)
+        digit2 = Decimal(digit2)
+
+    except (EmptyFieldError, DivZeroError, InvalidOperation) as exception:
+        return render_template('message.html', exception = exception.args[0])
+
+    # Calculation
+    operation_kind = {'+': digit1 + digit2,
+                      '-': digit1 - digit2,
+                      '*': digit1 * digit2,
+                      '/': digit1 / digit2}
+    for n in operation_kind:
+        if n == operation:
+            summ = operation_kind[n]
+
+    # Output
+    message = "The result is: {0:.4g}".format(summ)
+    return render_template('message.html', message = message)
+
+if __name__ == '__main__':
+    app.run()
